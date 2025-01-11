12842dc03fdf7092a38c04149d06e5077ccd2106
toor-4 <70628496+toor-4@users.noreply.github.com>
2025-01-11 23:09:08 +0000

    Encryption  JWT  implementation

--- app.js
+++ app.js
@@ -1,9 +1,58 @@
+require("dotenv").config()
 const express = require("express")
 const app = express()
-require("dotenv").config()
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+const User = require("./models/user")
+
+app.use(express.json())
 
 app.get("/" , (req, res) => {
     res.send("<h1>Github CodeSpace</h1>")
+})
+
+app.post("/register", async (req, res) => {
+  try {
+    // get all data from body
+    const { name, email, password } = req.body
+    // all data should exists
+    if (!(name && email && password)) {
+      req.status(404).json({ "message" : "All fields should be filled up."})
+    }
+    // check if iser already exists
+    const existingUser = await User.findOne({email})
+    if (existingUser) {
+      req.status(401).send("User already exists with this email.")
+    }
+    // encrypt the passwd
+    const encryptedPassword = await bcrypt.hash(password,10)
+    // save the user in the database
+    const user = await User.create({ 
+      name,
+      email,
+      password: encryptedPassword
+    })
+    // generate a token for a user and send it
+    const token =  jwt.sign(
+      { id: user._id, name},
+      "shhhh" // process.env.jwtsecret
+    ,
+    {
+    expiresIn: "3hr",
+    }
+  );
+  user.token = token
+  user.password = undefined
+  
+  res.status(201).json({
+    "status": "success",
+    token,
+    user
+  })
+  } catch (error) {
+    console.log(error)
+      }
 })
 
 module.exports = app
