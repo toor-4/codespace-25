702f74b9df1bf7318687e50a45e572a2f6724e07
toor-4 <70628496+toor-4@users.noreply.github.com>
2025-01-11 21:19:07 +0000

    create user model

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        match: [/.+\@.+\..+/, 'Please fill a valid email address']
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    }
+}, {
+    timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
